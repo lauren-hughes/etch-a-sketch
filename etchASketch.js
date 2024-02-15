c48c0875b6b66b8a558b754037940316b1e4c90c
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-16 01:41:39 +1100

    Add drawing effect to grid
    Added an event listener to each square that changes the background
    colour to black when it is hovered over. This creates a drawing effect.
    The CSS was modified to prevent the need for scrolling to see the whole
    drawing pad.

--- etchASketch.js
+++ etchASketch.js
@@ -1,9 +1,11 @@
 const container = document.querySelector("#container");
 
+// Creates rows
 for (let i = 0; i < 16; i++) {
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("row");
 
+    // Creates divisions within each row
     for (let j = 0; j < 16; j++) {
         let squareDiv = document.createElement("div");
         squareDiv.classList.add("square");
@@ -12,3 +14,7 @@ for (let i = 0; i < 16; i++) {
 
     container.appendChild(rowDiv);
 }
+
+const squares = document.querySelectorAll(".square");
+// When each square is hovered over, the class filled is added to it, which changes the background color to black
+squares.forEach(square => square.addEventListener("mouseenter", event => event.target.classList.add("filled")));
