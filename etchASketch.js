164377a8fe908c134988d654da0792898d7ed479
Lauren Hughes <lauren_hughes@outlook.com.au>
2024-02-18 18:46:49 +1100

    Randomise RGB value of squares
    Added a changeColour function so that when hovered over the squares
    change to a random colour rather than just black. The colour does not
    change for subsequent hovers.

--- etchASketch.js
+++ etchASketch.js
@@ -22,8 +22,23 @@ function createGrid(n) {
     }
 
     const squares = document.querySelectorAll(".square");
-    // When each square is hovered over, the class filled is added to it, which changes the background color to black
-    squares.forEach(square => square.addEventListener("mouseenter", event => event.target.classList.add("filled")));
+    // When each square is hovered over, the background colour is changed randomly
+    squares.forEach(square => square.addEventListener("mouseenter", changeColour));
+}
+
+function changeColour(event) {
+    let square = event.target;
+
+    // Background colour should only be changed if it hasn't been changed before
+    // There is a small chance that the random background colour is white, so can't just check the background colour
+    if (square.classList.contains("filled")) {
+        console.log("filled");
+    }
+    else {
+        // Changes square background color to a random colour
+        square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        square.classList.add("filled");
+    }
 }
 
 function changeGrid() {
